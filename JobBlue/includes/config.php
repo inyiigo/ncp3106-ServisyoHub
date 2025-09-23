67b69bbc30bd068ee720f7718f2685e0ef0ed6bc
inyiigo <ramos.francoinigo@ue.edu.ph>
2025-09-23 22:48:34 +0800

    Delete JobBlue directory

--- JobBlue/includes/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Placeholder for future configuration (database, env, etc.)
-// Keep file for structure parity.
-
-
--- JobBlue/includes/footer.php
+++
@@ -1,4 +0,0 @@
-    <script src="assets/js/script.js" defer></script>
-  </body>
-</html>
-
